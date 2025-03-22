b16040311c41d1c3c264c06f394a8794c007671c
Gato24-tech <ivan.queralt@gmail.com>
2025-03-22 13:43:51 +0100

    Ajustes en interact.js

--- scripts/interact.js
+++ scripts/interact.js
@@ -3,18 +3,18 @@ const hre  = require ("hardhat");
 
 async function main() {
     // Conecto el contrato con owner y recipient
-    const [owner, recipient] = await hre.getSigners();
-    console.log("Address owner:", owner.address);
+    const [owner, recipient] = await hre.ethers.getSigners();
+    console.log("Address owner:", owner.adress);
     console.log("Address recipient:", recipient.address);
     
-    const contract = await hre.getContractAt("MyNFT", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
+    const contract = await hre.ethers.getContractAt("MyNFT", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
 
     // Leer información del contrato (ver saldo)
     const balance = await contract.getBalance();
-    console.log("saldo actual:", hre.formatEther(balance), "ETH");
+    console.log("saldo actual:", hre.ethers.formatEther(balance), "ETH");
 
     // Realizamos una transferencia 
-    const tx = await contract.deposit({ value: hre.parseEther("1")});
+    const tx = await contract.deposit({ value: hre.ethers.parseEther("1")});
     await tx.wait();
     console.log("Deposito realizado!");
 
