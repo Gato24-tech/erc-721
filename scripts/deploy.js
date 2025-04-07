8aeb6eaca7aeb379d6048999a32918e70126c4af
Gato24-tech <ivan.queralt@gmail.com>
2025-04-07 17:28:23 +0200

    MyDeploy.json, cambios en deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,8 @@
 const hre = require("hardhat");
+const Path = require("path");
+const fs = require("fs");
+const path = require("path");
+
 
 async function main() {
     const [deployer] = await hre.ethers.getSigners();
@@ -8,8 +12,30 @@ async function main() {
     const myNFT = await MyNFT.deploy(10); // Por ejemplo, máximo de 10 NFTs
     await myNFT.waitForDeployment();
 
-    console.log("MyNFT deployed to:", myNFT.target);
-    const deploymentsDir = Path.join(__dirname,"../deploments");
+    const contractAddress = await myNFT.getAddress();
+
+    console.log("MyNFT deployed to:", contractAddress);
+    const deploymentsDir = Path.join(__dirname,"../deploments"); 
+
+    if (!fs.existsSync(deploymentsDir)) {
+       fs.mkdirSync(deploymentsDir);
+    }
+
+        // Guardar la dirección en formato JSON
+        const data = {
+            address: contractAddress,
+            network: hre.network.name,
+            timestamp: new Date().toISOString()
+        };
+
+    fs.writeFileSync(
+        path.join(deploymentsDir,"MyDeploy.js"),
+        JSON.stringify({address: contractAddress}, null, 2)
+        
+    );
+
+    console.log("Dirección guardada en deployments/MyDeploy.json");
+
 }
 
 main().catch((error) => {
