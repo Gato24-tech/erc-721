92b479927dbbbbb7a69627f4a3340d2a2e65cfa2
Gato24-tech <ivan.queralt@gmail.com>
2025-04-15 17:59:34 +0200

    Cambios en hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,11 @@ module.exports = {
     networks: {
       localhost: {
         url: "http://127.0.0.1:8545"
-      }
+      },
+      arbitrumSepolia: {
+        url: `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+        accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      },
     }
   }
   
