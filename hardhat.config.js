b53743c71f4ac89d1ce0e566bb3494080a39200f
Gato24-tech <ivan.queralt@gmail.com>
2025-04-18 23:35:03 +0200

    hardhat.config.js, test-env.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
+
 module.exports = {
   solidity: {
     version: "0.8.28",
@@ -17,8 +19,8 @@ module.exports = {
       url: "http://127.0.0.1:8545",
     },
     arbitrumSepolia: {
-      url: `https://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      url: `https://arb-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
   },
 };
---
+++ test-env.js
@@ -0,0 +1,4 @@
+// test-env.js
+require("dotenv").config();
+
+console.log("PRIVATE_KEY:", process.env.PRIVATE_KEY);
